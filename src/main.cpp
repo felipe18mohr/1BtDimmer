0e490499c4663bb6a6470c6f283657988119d8a1
Felipe Mohr <50018670+felipe18mohr@users.noreply.github.com>
2020-10-11 15:59:59 -0300

    Inprog. Not working yet

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,64 @@
-#include <mbed.h>
+#include "mbed.h"
+
+DigitalOut orange(p5);
+DigitalOut blue(p6);
+InterruptIn but(p7);
+PwmOut led(p9);
+Timeout time_pressed;
+
+enum states{DOWN, UP, FALL, MIN, RISE, MAX};
+int state = UP;
+bool pressed;
+
+void but_rise();
+void but_fall();
+void intensity();
 
 int main() {
+    blue = 0;
+    orange = 0;
+    led = 1;
+    but.rise(&but_rise);
+    but.fall(&but_fall);
+    while(1){
+        printf("Estado: %d\n", state);
+        wait_ms(50);
+    }
+}
 
-  // put your setup code here, to run once:
+void but_rise(){
+    pressed = true;
+    time_pressed.attach(&intensity, 1.0);
+}
 
-  while(1) {
-    // put your main code here, to run repeatedly:
-  }
+void but_fall(){
+    pressed = false;
+    if ((state==DOWN) || (state == MIN) || ((state == FALL) && (led.read() > 0.0))){
+        blue = 1;
+        orange = 0;
+        state = UP;
+    }
+    else if((state==UP) || (state == MAX) || ((state == RISE) && (led.read() < 1.0))){
+        orange = 1;
+        blue = 0;
+        state = DOWN;
+    }
+}
+
+void intensity(){
+    if (pressed){
+        if((state == RISE) && (led.read() == 1.0)) state = MAX;
+        else if((state == FALL) && (led.read() == 0.0)) state = MIN;
+        else{
+        
+            if((state == UP) || (state == RISE)){
+                state = RISE;
+                if(led.read() <= 0.95) led = led + 0.05;
+            }
+            if((state == DOWN) || (state == FALL)){
+                state = FALL;
+                if(led.read() >= 0.05) led = led - 0.05;
+            }
+        }
+    }
 }
